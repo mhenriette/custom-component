8c528f75bf520e8d242150fa01a494bc32665f0f
mhenriette <mhenriette00@gnmail.com>
2023-03-09 17:00:10 +0200

    feat: custom button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ number }) => {
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        alert(`Clicked button ${number}`)
+    }
+
+    return <div className="btn_container">
+        <button className="btn" onClick={handleClick}>{`Button ${number}`}</button>
+    </div>;
+};
+
+export default Button;
